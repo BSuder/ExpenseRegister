4da0613267296e3901433b926e08d0917aaab1ae
Unknown <suderbartek@gmail.com>
2018-04-26 21:23:49 +0200

    dodawanie kategorii i zarzadzanie nimi finished

--- www/js/frontEndHelper.js
+++ www/js/frontEndHelper.js
@@ -103,109 +103,119 @@
 	cell2.innerHTML = "<b>" +test+  "</b>";	
   } 
 
-  function printHistory(){
+	function printHistory(){
 	  
-	var jsonDataForBrands='[{"category":"dochod","value":"a@b"},{"category":"odchod","value":"b@c"},{"category":"gowno","value":"c@d"}]';
-	var Categories=JSON.parse(jsonDataForBrands);
+		var jsonDataForBrands='[{"category":"dochod","value":"a@b"},{"category":"odchod","value":"b@c"},{"category":"gowno","value":"c@d"}]';
+		var Categories=JSON.parse(jsonDataForBrands);
 
-	var table = document.getElementById("historyTable");
+		var table = document.getElementById("historyTable");
 
-	var tableCnt = $('#historyTable tr').length;
+		var tableCnt = $('#historyTable tr').length;
 
-	// delete all inside rows
-	for(var i=1; i<tableCnt; i++){
-		table.deleteRow(1);			
+		// delete all inside rows
+		for(var i=1; i<tableCnt; i++){
+			table.deleteRow(1);			
+		}
+
+		// print new inside rows
+		for (var catName in Categories) {
+			// Create an empty <tr> element and add it to the 1st position of the table:
+			var row = table.insertRow(1);
+			// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+			var cell1 = row.insertCell(0);
+			var cell2 = row.insertCell(1);
+			var cell3 = row.insertCell(2);
+			var cell4 = row.insertCell(3);
+			// Add some text to the new cells:
+			cell1.innerHTML = Categories[catName].category;
+			cell2.innerHTML = Categories[catName].value;
+			cell3.innerHTML = Categories[catName].value;
+			cell4.innerHTML = Categories[catName].value;
+		}
+	
 	}
+  
+	function sendBalance(){
+		//	pobranie z frontu danych
+		var tmp = document.getElementById("balance").value;
+		// zamiana przecinka na kropke
+		tmp = repleaceComma(tmp);
+		//	sprawdzenie czy wpisana zmienna do pola amount jest liczbą
+		wydatek = parseFloat(tmp);
+		if(isNaN(tmp)){
+			alert("Error, value in Amount of effort must be a number");
+		}
+		else{
+			return printData();
+		}
+		// to do wysłanie danych
+	}
+  
+	function manageCategories(){
 
-	// print new inside rows
-	for (var catName in Categories) {
-		// Create an empty <tr> element and add it to the 1st position of the table:
+		var table = document.getElementById("Categories");
+		var tableCnt = $('#Categories tr').length;
+		
+		// delete all inside rows
+		for(var i=1; i<tableCnt; i++){
+			table.deleteRow(1);			
+		}
+		
+		// dodanie ostatniej linijki
+		var helperChioce ="<select id=\"newCategoryType\"><option value=\"Income\">Income</option><option value=\"Effort\">Effort</option></select>";
+		var helperButton = "<button onclick=SaveNewCategory()><img src=\"img/save.png\" /></button>";
+		
 		var row = table.insertRow(1);
 		// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
 		var cell1 = row.insertCell(0);
 		var cell2 = row.insertCell(1);
 		var cell3 = row.insertCell(2);
-		var cell4 = row.insertCell(3);
-		// Add some text to the new cells:
-		cell1.innerHTML = Categories[catName].category;
-		cell2.innerHTML = Categories[catName].value;
-		cell3.innerHTML = Categories[catName].value;
-		cell4.innerHTML = Categories[catName].value;
-	}
-	
-  }
-  
-  function sendBalance(){
-	  
-	//	pobranie z frontu danych
-	var tmp = document.getElementById("balance").value;
-	// zamiana przecinka na kropke
-	tmp = repleaceComma(tmp);
-	//	sprawdzenie czy wpisana zmienna do pola amount jest liczbą
-	wydatek = parseFloat(tmp);
-	if(isNaN(tmp)){
-		alert("Error, value in Amount of effort must be a number");
-	}
-	else{
-		return printData();
-	}
-	// to do wysłanie danych
-  
-  }
-  
-  function manageCategories(){
-
-	var table = document.getElementById("Categories");
-	var tableCnt = $('#Categories tr').length;
-	
-	// delete all inside rows
-	for(var i=1; i<tableCnt; i++){
-		table.deleteRow(1);			
-	}
-	
-	// print new inside rows
-	for (var catName in Cats) {
-		catName++;
-		// Create an empty <tr> element and add it to the 1st position of the table:
-		var row = table.insertRow(catName);
-		// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-		var cell1 = row.insertCell(0);
-		var cell2 = row.insertCell(1);
-		var cell3 = row.insertCell(2);
 		
-		var tmp;
-		if(Cats[catName].type == -1){
-			tmp = "Effort";
+		cell1.innerHTML = "<input id=\"newCategoryName\"></input>";
+		cell2.innerHTML = helperChioce;
+		cell3.innerHTML = helperButton;
+		
+		// print new inside rows
+		for (var catName in Cats) {
+			catName++;
+			// Create an empty <tr> element and add it to the 1st position of the table:
+			var row = table.insertRow(catName);
+			// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+			var cell1 = row.insertCell(0);
+			var cell2 = row.insertCell(1);
+			var cell3 = row.insertCell(2);
+			
+			var tmp;
+			if(Cats[catName].type == -1){
+				tmp = "Effort";
+			}
+			else{
+				tmp = "Income";
+			};
+			
+			// Add some text to the new cells:
+			cell1.innerHTML = Cats[catName].category;
+			cell2.innerHTML = tmp;
+			cell3.innerHTML = "<button onclick=deleteCategory("+catName+")><img src=\"img/del.png\" /></button>";
 		}
-		else{
-			tmp = "Income";
-		};
-		
-		// Add some text to the new cells:
-		cell1.innerHTML = Cats[catName].category;
-		cell2.innerHTML = tmp;
-		cell3.innerHTML = "<button onclick=deleteCategory("+catName+")><img src=\"img/del.png\" /></button>";
+
+	}
+	
+	function deleteCategory(btn){
+		// usuniecie kategorii 
+		Cats.splice(Cats[btn] - 1, 1);
+		// przeładowanie obecnych kategroii
+		manageCategories();
 	}
 
-	// add last row to add new category
-	alert("wcisnieto przywcisk:");
-	// dodanie ostatniej linijki
-	var row = table.insertRow(1);
-	// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-	var cell1 = row.insertCell(0);
-	var cell2 = row.insertCell(1);
-	var cell3 = row.insertCell(2);
-	
-	cell1.innerHTML = "aaaa";
-	cell2.innerHTML = "test";
-	cell3.innerHTML = "bbb";
-
-  }
-	
-function deleteCategory(btn){
-	// usuniecie kategorii 
-	Cats.splice(Cats[btn] - 1, 1);
-	// przeładowanie obecnych kategroii
-	manageCategories();
-}
-  
+	function SaveNewCategory(){
+		var CategotyName = document.getElementById("newCategoryName").value;
+		var CategoryType = document.getElementById("newCategoryType").value;
+		
+		if(CategoryType == "Income"){CategoryType = 1;}
+		else{CategoryType = -1;}
+		
+		var helperJSON = {"category":CategotyName, "type":CategoryType};
+		Cats.push(helperJSON);
+		manageCategories();
+	}
